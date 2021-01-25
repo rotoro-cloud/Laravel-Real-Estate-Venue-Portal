6076f46b1062be0479acfc5df1af8e4e056fd99d
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2021-01-25 12:59:25 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        \URL::forceScheme('https');
         View::composer('*', function ($view) {
             $view->with('globalLocations', Location::all());
             $view->with('globalEventTypes', EventType::all());
